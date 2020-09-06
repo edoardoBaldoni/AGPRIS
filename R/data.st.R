f336738006bf9ac3a6d1d01fdbc23dd40f0b1f4a
edoardoBaldoni <edoardo.baldoni@gmail.com>
2020-09-06 23:31:45 +0200

    OK I am trying to committ

---
+++ R/data.st.R
@@ -0,0 +1,7 @@
+#' Macro (NUTS3) Panel 
+#'
+#' A STDFDF object containing agricultural TFP relative levels and other attributes of NUTS3 regions in Italy. Data of NUTS3 regions with a low number of sampled farms (<15) have been artificially altered to preserve confidentiality.
+#'
+#' @format A STFDF 
+#' 
+"data.st"
